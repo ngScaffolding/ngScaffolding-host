16789fdcf234a21da23fe8b6b50aef696bd22eea
Darren Baines <dbaines1@hotmail.com>
2018-03-08 20:16:36 +0000

    Prefs

--- src/modules/core/services/userPreferences/userPreferences.service.ts
+++ src/modules/core/services/userPreferences/userPreferences.service.ts
@@ -81,7 +81,8 @@ export class UserPreferencesService {
   }
 
   public setValue(key: string, value: any) {
-    this.http.post(`${this.apiRootValues}`, {name: key, value: value});
+    this.http.post(`${this.apiRootValues}`, {name: key, value: value})
+    .subscribe();
 
     // Save and tell the world
     this.newValue(key, value);
