9296d6457c81c4ddfa246ae1ca919e42d6c28521
dbaines30 <dbaines1@hotmail.com>
2018-05-18 07:54:16 +0100

    Dashboard Module

--- src/modules/dashboard/dashboardModule.ts
+++ src/modules/dashboard/dashboardModule.ts
@@ -1,51 +1,73 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { VERSION } from './version';
+import { HttpClientModule } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 
-import { RouterModule, Routes } from '@angular/router';
+import { ActionService } from './services/action/action.service';
+import { AppSettingsService } from './services/appSettings/appSettings.service';
+import { UserAuthorisationService } from './services/userAuthorisation/userAuthorisation.service';
+import { BroadcastService } from './services/broadcast/broadcast.service';
+import { CacheService } from './services/cache/cache.service';
+import { CoreErrorHandlerService } from './services/coreErrorHandler/coreErrorHandler.service';
+import { LoggingService } from './services/logging/logging.service';
+import { MenuService } from './services/menu/menu.service';
+import { DataSourceService } from './services/dataSource/dataSource.service';
+import { NotificationService } from './services/notification/notification.service';
+import { ReferenceValuesService } from './services/referenceValues/refrenceValues.service';
+import { UserPreferencesService } from './services/userPreferences/userPreferences.service';
+import { SpinnerService } from './services/spinnerService/spinner.service';
+import { RolesService } from './services/rolesService/roles.service';
+import { VersionsService } from './services/versions/versions.service';
 
-import { CoreModule, MenuService, LoggingService, VersionsService } from '../core/coreModule';
-import { ButtonColorPipe } from '../core/coreModule';
-import { GridsterModule } from 'angular-gridster2';
+// Pipes
+import { ButtonColorPipe } from './pipes/index';
 
-import { InputBuilderModule } from '../inputbuilder/inputbuilderModule';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+export * from './services/index';
+export * from './routeGuards/index';
+export * from './models/index';
+export * from './pipes/index';
 
-// Services
-
-const appRoutes: Routes = [
-  { path: 'dashboard/:id', component: DashboardComponent },
-  { path: 'dashboard', component: DashboardComponent }
-];
+// Interfaces
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    InputBuilderModule,
-    CoreModule,
-    GridsterModule,
-    RouterModule.forChild(appRoutes)
-  ],
+  imports: [CommonModule, HttpClientModule],
   declarations: [
-    DashboardComponent
+    // SampleComponent,
+    // SampleDirective,
+    // Pipes
+    ButtonColorPipe
   ],
   exports: [
-    DashboardComponent,
-    RouterModule
-  ],
-  providers: [
+    // SampleComponent,
+    // SampleDirective,
+    // Pipes
+    ButtonColorPipe
   ]
 })
-export class DashboardModule {
+export class CoreModule {
   static forRoot(): ModuleWithProviders {
     return {
-      ngModule: DashboardModule
+      ngModule: CoreModule,
+      providers: [
+        ActionService,
+        AppSettingsService,
+        DataSourceService,
+        UserAuthorisationService,
+        BroadcastService,
+        CacheService,
+        CoreErrorHandlerService,
+        LoggingService,
+        NotificationService,
+        MenuService,
+        ReferenceValuesService,
+        RolesService,
+        UserPreferencesService,
+        SpinnerService,
+        VersionsService
+      ]
     };
   }
-
-  constructor(menuService: MenuService, logger: LoggingService, versions: VersionsService) {
-    versions.addVersion('@ngscaffolding/dashboard', VERSION.version);
+  constructor(versions: VersionsService) {
+    versions.addVersion('@ngscaffolding/core', VERSION.version);
   }
 }
