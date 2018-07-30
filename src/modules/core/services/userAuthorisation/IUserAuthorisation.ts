2d2acf0190932f619ca2285cde489dd9ab58bca3
Darren Baines <dbaines1@hotmail.com>
2018-07-30 22:15:54 +0100

    BaseUserAuth

--- src/modules/core/services/userAuthorisation/IUserAuthorisation.ts
+++
@@ -1,7 +0,0 @@
-export interface IUserAuthorisationService {
-  isAuthenticated(): boolean;
-  setToken(token: any);
-  getToken(): string;
-  logon(userName: string, password: string);
-  logoff();
-}
