bbbcb1770ba749544fdf97ef7055d4fa7fe6239b
Darren Baines <dbaines1@hotmail.com>
2019-07-01 17:30:43 +0100

    Remove Projects

--- projects/ngscaffolding-chart/src/public_api.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Public API Surface of ngscaffolding-core
- */
-
-
-export * from './ngscaffolding-chart.module';
-
-export * from './services';
-export * from './components';
--- projects/ngscaffolding-datagrid/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngscaffolding-datagrid
- */
-
-export * from './ngscaffolding-datagrid.module';
-
-export * from './components';
