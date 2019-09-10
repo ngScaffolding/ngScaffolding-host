2b28837a8579398873437b80ce2e673d91010f29
Darren Baines <darren.baines@netwealth.com>
2019-09-10 16:56:10 +0100

    Remove Demo Code

--- custom/custom.app.ts
+++ custom/custom.app.ts
@@ -1,7 +1,6 @@
  import { DemoAppModule } from '../src/modules/demoapp/demoappModule';
- import { FieldforceModule } from '../src/modules/fieldforce/fieldforceModule';
 
 export const CUSTOM_IMPORTS: any[] = [
-  FieldforceModule.forRoot()
+  DemoAppModule.forRoot()
 ];
 export const CUSTOM_COMPONENTS: any[] =  [];
