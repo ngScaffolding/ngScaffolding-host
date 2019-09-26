6c064459f757587fd8e74d072b73d1d68b59aede
Darren Baines <dbaines1@hotmail.com>
2019-09-26 17:33:55 +0100

    Correct loading of Reference Vals

--- custom/custom.app.ts
+++ custom/custom.app.ts
@@ -1,6 +1,6 @@
- import { DemoAppModule } from '../src/modules/demoapp/demoappModule';
+ import { FieldforceModule } from '../src/modules/fieldforce/fieldforceModule';
 
 export const CUSTOM_IMPORTS: any[] = [
-  DemoAppModule.forRoot()
+  FieldforceModule.forRoot()
 ];
 export const CUSTOM_COMPONENTS: any[] =  [];
