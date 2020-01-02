539abddf096f731938da402bbda4072f80845c2f
Darren Baines <dbaines1@hotmail.com>
2020-01-02 12:35:21 +0000

    SQL Stored Procs Done

--- projects/core/services/action/action.service.ts
+++ projects/core/services/action/action.service.ts
@@ -9,34 +9,32 @@ import { ActionResultModel, ActionRequestModel } from 'ngscaffolding-models';
 import { UserAuthenticationQuery } from '../userAuthentication';
 import { UserAuthenticationService } from '../userAuthentication';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class ActionService {
-  constructor(private http: HttpClient, private appSettingsService: AppSettingsService, private authQuery: UserAuthenticationQuery) {}
+    constructor(
+        private http: HttpClient,
+        private appSettingsService: AppSettingsService,
+        private authQuery: UserAuthenticationQuery
+    ) {}
 
-  callAction(action: Action, inputDetails: any, rows: any[], baseContext: object): Observable<ActionResultModel> {
-    // Add in base Context
-    if (baseContext) {
-      inputDetails = { ...baseContext, ...inputDetails };
-    } else {
-      // Clone so we can extend
-      inputDetails = { ...inputDetails };
+    callAction(action: Action, inputDetails: any, rows: any[], baseContext: object): Observable<ActionResultModel> {
+        // Add in base Context
+        if (baseContext) {
+            inputDetails = { ...baseContext, ...inputDetails };
+        } else {
+            // Clone so we can extend
+            inputDetails = { ...inputDetails };
+        }
+
+        const request: ActionRequestModel = {
+            action: action,
+            inputDetails: inputDetails,
+            rows: rows
+        };
+
+        return this.http.post<ActionResultModel>(
+            `${this.appSettingsService.getValue(AppSettings.apiHome)}/api/v1/action`,
+            request
+        );
     }
-    // Add in standard Values
-    const currentUser = this.authQuery.getValue().userDetails;
-    const now = new Date();
-    inputDetails['now'] = now;
-    inputDetails['zuluDate'] = new Date(Date.UTC(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours(), now.getMinutes(), now.getSeconds()));
-
-    inputDetails['userId'] = currentUser.userId;
-
-    const request: ActionRequestModel = {
-      action: action,
-      inputDetails: inputDetails,
-      rows: rows
-    };
-
-    return this.http.post<ActionResultModel>(`${this.appSettingsService.getValue(AppSettings.apiHome)}/api/v1/action`, request);
-  }
 }
