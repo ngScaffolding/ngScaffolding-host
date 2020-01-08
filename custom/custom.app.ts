385eded0f3e06d11f5d0d86bc1843cdbdda4cfc4
Darren Baines <darren.baines@netwealth.com>
2020-01-08 07:48:03 +0000

    Move demoModule

--- custom/custom.app.ts
+++ custom/custom.app.ts
@@ -1,8 +1,6 @@
- import { FieldforceModule } from '../src/modules/fieldforce/fieldforceModule';
- import { DemoAppModule } from '../src/modules/demoapp/demoappModule';
+ import { DemoAppModule } from '../src/app/modules/demoapp/demoappModule';
 
 export const CUSTOM_IMPORTS: any[] = [
-  DemoAppModule.forRoot(),
-  FieldforceModule.forRoot()
+  DemoAppModule.forRoot()
 ];
 export const CUSTOM_COMPONENTS: any[] =  [];
